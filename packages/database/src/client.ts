f3140d2199fade8acad7e03b5a417d165de9bd46
Alex Padilla <alex.padilla@epn.edu.ec>
2025-01-13 16:31:13 -0500

    chore: update prisma generation

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -1,11 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/client";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+export const db: PrismaClient = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
 
-export * from "@prisma/client";
+export * from "./generated/client";
